3ebc7a42cd48541c3ba581b83485532429ab693c
kawamataryou <ba068082@gmail.com>
2020-03-17 18:12:14 +0900

    [from now] 2020/03/17 18:12:14
    diff --git a/functions/src/index.ts b/functions/src/index.ts
    index 2204ef4..3e561ca 100644
    --- a/functions/src/index.ts
    +++ b/functions/src/index.ts
    @@ -1,8 +1,4 @@
     import * as functions from "firebase-functions";
     import { expressReceiver } from "./slack/app";
    
    -const REGION = "asia-northeast1";
    -
    -export const slack = functions
    -  .region(REGION)
    -  .https.onRequest(expressReceiver.app);
    +export const slack = functions.https.onRequest(expressReceiver.app);

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,4 @@
 import * as functions from "firebase-functions";
 import { expressReceiver } from "./slack/app";
 
-const REGION = "asia-northeast1";
-
-export const slack = functions
-  .region(REGION)
-  .https.onRequest(expressReceiver.app);
+export const slack = functions.https.onRequest(expressReceiver.app);
